687b2dabc060a33ddacc8f0833813a1a6f7d55b5
Sohel Rana <73846274+SohelRana-CSE@users.noreply.github.com>
2024-02-13 20:46:15 +0600

    Add files via upload

---
+++ Class-01_Practice.js
@@ -0,0 +1,61 @@
+console.log("First Class");
+console.log("First Class");
+
+var mangoPrice = 10;
+console.log(mangoPrice);
+
+var mangoPrice = 10;
+console.log(typeof mangoPrice);
+
+var myName = "Sohel";
+console.log(typeof myName);
+
+var isGood = true;
+console.log(typeof isGood);
+
+var isBad = false;
+console.log(typeof isBad);
+
+var myCountry = "I Love Bangladesh";
+myCountry.toLowerCase();
+console.log(myCountry);
+
+var myCountry = "I Love Bangladesh";
+var lowerCase = myCountry.toLowerCase();
+console.log(lowerCase);
+var upperCase = myCountry.toUpperCase();
+console.log(upperCase);
+var position = myCountry.indexOf("Love");
+console.log(position);
+var split = myCountry.split(" ");
+console.log(split);
+
+var num = 10;
+var num2 = 5;
+var total = num + num2;
+console.log(total);
+
+var num = 10;
+var num2 = parseInt('5');
+var total = num + num2;
+console.log(total);
+
+var num = 10;
+var num2 = parseFloat('5.50');
+var total = num + num2;
+console.log(total);
+
+var num1 = 10;
+var num2 = 6;
+var add = num1 + num2;
+var sub = num1 - num2;
+var multi = num1 * num2;
+var div = (num1 / num2).toFixed(2);
+var div = parseFloat(div);
+var reminder = num1 % num2;
+console.log(add, sub, multi, div, reminder);
+
+var firstName = "Sohel";
+var lastName = "Rana";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
